f6ab18337da9c4442440e9925d14522ccf7067f3
SamuelSam-dev <xsamansafaeix@gmail.com>
2021-09-01 16:39:52 +0430

    Upgrade

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder="resources/templates")
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
