4f64b132c087688553ed35ed043a726097881380
SamuelSam-dev <xsamansafaeix@gmail.com>
2021-09-01 16:34:00 +0430

    Upgrade

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="./resources/templates")
+app = Flask(__name__, template_folder="resources/templates")
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
 
 app.run(port=4040, debug=True)
